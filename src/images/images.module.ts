3b06ff509e99b0660d30847dcd7dab946fa423f6
marekbielsky <marekbielski@yahoo.com>
2025-11-26 13:43:50 +0100

    chore(docker): add npm scripts for quick and full rebuild commands

--- src/images/images.module.ts
+++ src/images/images.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { S3Module } from '@app/s3/s3.module';
+
 import { ImagesController } from './controllers/images.controller';
 import { ImagesRepository } from './repositories/images.repository';
 import { ImagesService } from './services/images.service';
 
 @Module({
+  imports: [S3Module],
   controllers: [ImagesController],
   providers: [ImagesService, ImagesRepository],
   exports: [ImagesService],
