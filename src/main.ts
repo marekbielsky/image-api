6389cffda6178e4ce62bc64ba27a068d2494ba1a
marekbielsky <marekbielski@yahoo.com>
2025-11-26 04:03:06 +0100

    docs(swagger): add Swagger setup for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Image API')
+    .setDescription('API for uploading and retrieving images')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000);
 }
+
 bootstrap();
